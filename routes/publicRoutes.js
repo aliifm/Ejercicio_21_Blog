1fb550f927000f5989cd2aa4d8768edc71aec93b
Ali <dev.alifm@gmail.com>
2023-02-09 12:48:50 -0300

    probando habilitar db y ruteando home

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// Rutas relacionadas a la parte pública del sitio web:
-// ...
+router.get("/", (req, res) => {
+  res.render("home");
+});
 
 module.exports = router;
